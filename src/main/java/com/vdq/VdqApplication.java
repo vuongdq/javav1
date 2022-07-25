0c84f3c81a50c5a6036b7312d27cdc34ec7f7fa7
vuongdq <quocvuong_it@yahoo.com>
2022-07-25 09:48:48 +0700

    Add new Project

---
+++ src/main/java/com/vdq/VdqApplication.java
@@ -0,0 +1,13 @@
+package com.vdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vdq/VdqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
